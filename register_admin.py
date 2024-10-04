df466cfd346d8d1f077abe8b8a52292c60070f11
rubayet21itis <rubayet21itis@gmail.com>
2024-10-04 18:24:39 +0200

    yaml chnaged

--- register_admin.py
+++ register_admin.py
@@ -5,7 +5,7 @@ import requests
 
 # Synapse server details
 #server_url = "http://localhost:8080"  # local
-server_url = "http://85.215.118.180:8081"  # remote
+server_url = "http://85.215.118.180"  # remote
 #shared_secret = "SP;j&7cAKeqqjtQS2fk1W#ejuiT:G&uaggV&E,;g8Mx:1Xl#^X"  # local
 shared_secret = "',D+V@s@p&eIjPy0Cp89=7*43_w;cUXOYIJ8e:6=U3rcM0:IUdw'"  # remote
 
